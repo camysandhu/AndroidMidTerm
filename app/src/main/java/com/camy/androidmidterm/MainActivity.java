7539fd92fe0be3bd77900a4df103addad4e37233
camysandhu <fmsharmn@gmail.com>
2019-12-02 17:06:00 -0500

    [Harman] some changes

--- app/src/main/java/com/camy/androidmidterm/MainActivity.java
+++ app/src/main/java/com/camy/androidmidterm/MainActivity.java
@@ -196,14 +196,15 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else
                         {
-                            String age= dateFormattar();
-                            if(Integer.parseInt(age)<18)
-                            {
-                                btnCal.setAlpha(.5f);
-                                btnCal.setClickable(false);
-
-                            }
-                            else {
+//                            String age= dateFormattar();
+//                            if(Integer.parseInt(age)<18)
+//                            {
+//                                btnCal.setAlpha(.5f);
+//                                btnCal.setClickable(false);
+//
+//
+//                            }
+//                            else {
 
 
                                 CRACustomer cra = new CRACustomer(Integer.parseInt(edSIN.getText().toString()),
@@ -222,7 +223,7 @@ public class MainActivity extends AppCompatActivity {
                                 startActivity(mIntent);
                             }
 
-                    }
+
                 }
 
             private Activity getActivity() {
@@ -238,7 +239,7 @@ public class MainActivity extends AppCompatActivity {
 //    }
 
     private void dateFormatTax() {
-        String myFormat = "dd-MMM-yyyy"; //In which you need put here
+        String myFormat = "yyyy-MMM-dd"; //In which you need put here
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
         taxFilling.setText(sdf.format(calendarTax.getTime()));
     }
@@ -265,25 +266,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private String dateFormattar() {
+    private void dateFormattar() {
         String myFormat = "dd-MMM-yyyy";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
         txtDateOfBirth.setText(sdf.format(calendar.getTime()));
 
-        LocalDate l = LocalDate.of(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-        LocalDate now = LocalDate.now(); //gets localDate
-        Period diff = Period.between(l, now); //difference between the dates is calculated
-
-
-        String n1=String.valueOf(diff.getYears());
-        String n2=String.valueOf(diff.getMonths());
-        String n3=String.valueOf(diff.getDays());
-        String age="Age: "+n1+"Years, "+n2+"Months, "+n3+"Days";
-
-
-        mainAge.setText(age);
-        return  n1;
+//        LocalDate l = LocalDate.of(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+//        LocalDate now = LocalDate.now(); //gets localDate
+//        Period diff = Period.between(l, now); //difference between the dates is calculated
+//
+//
+//        String n1=String.valueOf(diff.getYears());
+//        String n2=String.valueOf(diff.getMonths());
+//        String n3=String.valueOf(diff.getDays());
+//        String age="Age: "+n1+"Years, "+n2+"Months, "+n3+"Days";
+//
+//
+//        mainAge.setText(age);
+//        return  n1;
 
 
 
