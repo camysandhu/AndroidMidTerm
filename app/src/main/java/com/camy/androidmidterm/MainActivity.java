5be72e36bc9f0ad325973947b23d98027a59e11d
camysandhu <fmsharmn@gmail.com>
2019-12-02 13:23:28 -0500

    [Harman] txtDateOfBirth in MainActivity

--- app/src/main/java/com/camy/androidmidterm/MainActivity.java
+++ app/src/main/java/com/camy/androidmidterm/MainActivity.java
@@ -60,6 +60,35 @@ public class MainActivity extends AppCompatActivity {
         rrspMain = findViewById(R.id.txtRRSPContributed);
         grossIncome = findViewById(R.id.edGrossIncome);
 
+
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth)
+            {
+
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, monthOfYear);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                // DatePicker.getDatePicker().setMaxDate(System.currentTimeMillis());
+                // datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                dateFormat();
+            }
+        };
+
+        txtDateOfBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new DatePickerDialog(MainActivity.this, date, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+//                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+            }
+        });
+
+
     }
 
     public void radioAction()
@@ -135,18 +164,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-        @Override
-        public void onDateSet(DatePicker view, int year, int monthOfYear,
-                              int dayOfMonth)
-        {
 
-            calendar.set(Calendar.YEAR, year);
-            calendar.set(Calendar.MONTH, monthOfYear);
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-            // DatePicker.getDatePicker().setMaxDate(System.currentTimeMillis());
-            // datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
-            dateFormat();
-        }
-    };
+
+
 }
