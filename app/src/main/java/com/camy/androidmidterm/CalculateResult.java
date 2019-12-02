da78ebfec971825fab7d4faacf25a1255d028b66
camysandhu <fmsharmn@gmail.com>
2019-12-02 12:56:37 -0500

    [Harman] creating calculateResult activity

---
+++ app/src/main/java/com/camy/androidmidterm/CalculateResult.java
@@ -0,0 +1,14 @@
+package com.camy.androidmidterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CalculateResult extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_result);
+    }
+}
