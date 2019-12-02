226f2e69d1d67cdb22d04e9764e7a7441137cd74
camysandhu <fmsharmn@gmail.com>
2019-12-02 13:32:07 -0500

    [Harman] Declare Varibales in CalculatedResultActivity

--- app/src/main/java/com/camy/androidmidterm/CalculateResult.java
+++ app/src/main/java/com/camy/androidmidterm/CalculateResult.java
@@ -3,8 +3,28 @@ package com.camy.androidmidterm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CalculateResult extends AppCompatActivity {
+    private TextView calculatedfullName;
+    private TextView calSIN;
+    private TextView calAge;
+    private TextView calGender;
+    private TextView calDOB;
+    private TextView calTaxFillingDate;
+
+    private TextView  txtDgrossIncome;
+    private TextView txtDfederalTax;
+    private TextView txtDprovincialTax;
+    private TextView txtCPP;
+    private TextView txtEmpInsurance;
+    private TextView txtRRSPContribution;
+    private TextView txtRRSPCarryForward;
+    private TextView txtTaxableIncome;
+    private TextView txtTaxPaid;
+
+    private double cpp = 0, ei = 0;  double rrsp = 0, rrspCf = 0, taxableIncome, federalTax,
+            provincialTax, totalTaxPaid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
